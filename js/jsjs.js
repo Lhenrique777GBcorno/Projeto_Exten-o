b1778142577b03c6124136dda76036522ae0f16c
Lhenrique777 <161779839+Lhenrique777GBcorno@users.noreply.github.com>
2024-11-15 18:13:01 -0300

    Add files via upload

---
+++ js/jsjs.js
@@ -0,0 +1,13 @@
+function onChangeEmail (){
+
+    toggleButtonDisable()
+    toggleEmailErrors()
+}
+function onChangeTelefone() {
+     toggleButtonDisable()
+     toggleTelefonedErros()
+}
+function onChangeText(){
+    toggleButtonDisable()
+    toggleText()
+}
